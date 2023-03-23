6761b0e65dfcfa151c781efb80e2729470b3ab74
conanhh <128455450+conanhh@users.noreply.github.com>
2023-03-22 20:50:30 -0400

    Added the decode() function and made minor formatting changes.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#Ian DeLano
+# Ian DeLano
 
-#Encoder for password
+# Encoder for password
 def encode(password):
     EncodedPassword = ""
     for Digit in password:
@@ -10,7 +10,8 @@ def encode(password):
         EncodedPassword += str(NewDigit)
     return EncodedPassword
 
-#Decoder for password
+
+# Decoder for password
 def decode(password):
     DecodedPassword = ""
     for Digit in password:
@@ -21,7 +22,7 @@ def decode(password):
     return DecodedPassword
 
 
-#Main Body which includes the menu and subsequent options
+# Main Body which includes the menu and subsequent options
 def main():
 
     while True:
@@ -30,18 +31,17 @@ def main():
 
         menu_option = int(input("Please enter an option: "))
 
-        if menu_option == 1:#Encodes the password
+        if menu_option == 1:  # Encodes the password
             password = input("Please enter your password to encode: ")
             encoded_password = encode(password)
             print("Your password has been encoded and stored!\n")
-        elif menu_option == 2:#Decodes the password
-            decoded_password= decode(encoded_password)
+        elif menu_option == 2:  # Decodes the password
+            decoded_password = decode(encoded_password)
             print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.\n")
-        elif menu_option == 3:#Exit the program
+        elif menu_option == 3:  # Exit the program
             break
 
 
-#Test Cases
+# Test Cases
 if __name__ == "__main__":
     main()
-
